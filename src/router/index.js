1dc211e7c0696fb4a16f164be179685f65135e2d
zxd <1099547678@qq.com>
2021-03-10 03:29:52 +0800

    封装路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,66 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../views/home.vue'
+import Index from '../views/index.vue'
+import Product from '../views/product.vue'
+import Detail from '../views/detail.vue'
+import Cart from '../views/cart.vue'
+import Order from '../views/order.vue'
+import OrderList from '../views/orderList.vue'
+import OrderConfirm from '../views/orderConfirm.vue'
+import OrderPay from '../views/orderPay.vue'
+import AliPay from '../views/alipay.vue'
+
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
+const routes = [{
     path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    name: 'home',
+    component: Home,
+    redirect: 'Index',
+    children: [{
+        path: '/index',
+        name: 'index',
+        component: Index
+    }, {
+        path: '/product/:id',
+        name: 'product',
+        component: Product
+    }, {
+        path: '/detail/:id',
+        name: 'Detail',
+        component: Detail
+    }]
+}, {
+    path: '/cart',
+    name: 'cart',
+    component: Cart
+}, {
+    path: '/order',
+    name: 'order',
+    component: Order,
+    children: [{
+        path: 'list',
+        name: 'order-list',
+        component: OrderList
+    }, {
+        path: 'confirm',
+        name: 'order-confirm',
+        component: OrderConfirm
+    }, {
+        path: 'pay',
+        name: 'order-pay',
+        component: OrderPay
+    }, {
+        path: 'alipay',
+        name: 'alipay',
+        component: AliPay
+    }]
+}]
 
 const router = new VueRouter({
-  routes
+    routes
 })
 
 export default router
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        home
+        <nav-header></nav-header>
+        <router-view></router-view>
+        <nav-footer></nav-footer>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+    import NavHeader from '../components/NavHeader'
+    import NavFooter from '../components/NavFooter'
+    export default{
+        name:'nav-home',
+        components:{
+            NavHeader,
+            NavFooter
+        }
+    }
 </script>
